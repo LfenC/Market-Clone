69b78c85075f316fb2f5f8b29a87c6a3b085c230
Santiago Melo <santiagocano15cr7@gmail.com>
2024-07-09 18:39:42 -0600

    build correct

--- src/types/uploaderUrlImageProps.ts
+++ src/types/uploaderUrlImageProps.ts
@@ -2,5 +2,5 @@ import { UploadResponse } from "imagekit/dist/libs/interfaces";
 
 export interface uploaderUrlImageProps {
     file: UploadResponse;
-    onClick: () => void;
+    onClick?: () => void;
 }
